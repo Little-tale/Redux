e7384ae70c66427ab918ba38dd00509932ca8c15
Kim JaeHyung <you3192001@icloud.com>
2024-11-21 14:56:28 +0900

    Initial Commit

---
+++ Redux/ReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxApp.swift
+//  Redux
+//
+//  Created by Jae hyung Kim on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
